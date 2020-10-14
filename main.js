959f912f60186ae3d3b51d6dcef8591a434b9888
Milda-Grabyte <milda.grabyte@gmail.com>
2020-10-14 09:52:54 +0200

    add phase 1

---
+++ main.js
@@ -0,0 +1,19 @@
+// PHASE 1
+
+//Write potato x 10
+
+function ten(word) {
+  console.log(word.repeat(10));
+}
+
+ten("patata ");
+
+// function repeatStringNumTimes(string, times) {
+//   let repeatedString = "";
+//   while (times > 0) {
+//     repeatedString += string;
+//     times--;
+//   }
+//   return repeatedString;
+// }
+// repeatStringNumTimes("patata ", 10);
